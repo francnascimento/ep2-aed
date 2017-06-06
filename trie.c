1aa88af6a026c9f45934dc237ec5007ab4d3ed2b
Gabriel Arruda <gabrielpardi@outlook.com>
2017-06-05 21:14:14 -0300

    Método 'adicioa' agora recebe textos com espaços.

--- trie.c
+++ trie.c
@@ -3,24 +3,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct auxi{
+  char **palavras;
+} TEXTO;
+
 typedef struct aux{
 
   struct aux **lista;
   char letra;
 } NO;
 
+
 void iniciaArv(NO *raiz){
   raiz->lista = (NO **) malloc(26*sizeof(NO));
 }
 
-void adicionaNo(NO *raiz, char letraN[]){
+
+void adicionaTexto(NO *raiz, char *text){
   int i, indice;
   NO *aux = raiz;
-  for(i = 0; i<strlen(letraN); i++){
-    indice = letraN[i] - 97;
+
+  for(i = 0; i<strlen(text); i++){
+    if(text[i] == ' '){
+      aux = raiz;
+      continue;
+    }
+    indice = text[i] - 97;
     if( !(aux->lista[indice]) ){
       aux->lista[indice] = (NO*) malloc(sizeof(NO));
-      aux->lista[indice]->letra = letraN[i];
+      aux->lista[indice]->letra = text[i];
       aux->lista[indice]->lista = (NO **) malloc(26*sizeof(NO));
     }
     aux = aux->lista[indice];
@@ -37,14 +48,36 @@ void imprime(NO *arv){
   }
 }
 
+void visu(NO *arv){
+  int i;
+  for(i = 0; i<26; i++){
+    if(arv->lista[i]){
+      printf("%c", arv->lista[i]->letra);
+    }
+  }
+  printf("\n");
+  for(i = 0; i<26; i++){
+    if(arv->lista[i]){
+      visu(arv->lista[i]);
+    }
+  }
+}
+
+
+
+
+
 void main(){
   NO *arv = (NO*) malloc(sizeof(NO));
   iniciaArv(arv);
-  adicionaNo(arv, "foi");
-  adicionaNo(arv, "fodasse");
-  adicionaNo(arv, "gabriel");
 
-  printf("impressao\n");
+
+  char *text = "forget your lust and the rich men gold";
+  char *text2 = "all that you need is in your soul";
+  adicionaTexto(arv, text);
+  adicionaTexto(arv, text2);
   imprime(arv);
   printf("\n");
+  //visu(arv);
+
 }
