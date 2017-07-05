5e6ac792afd9b9b85c38fc30c1e7becc1ec780d0
Gabriel Arruda <gabrielpardi@outlook.com>
2017-07-05 17:09:49 -0300

    KMP 2.2

---
+++ kmp.c
@@ -0,0 +1,101 @@
+#include <stdbool.h>   // variaveis bool assumem valores "true" ou "false"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+int* prefixos(char palavra[]){
+    int len = strlen(palavra);
+    int *prefix = (int*) calloc(len, sizeof(int));
+    //printf(palavra);
+    prefix[0] = 0;
+
+    int j = 0;
+    int i;
+
+    for(i = 1; i<len; i++){
+        prefix[i] = 0;
+        while(j>0 && palavra[j] != palavra[i]){
+            j = prefix[j-1];
+            //printf("%d %d\n", i, j);
+            //while(j>0 && palavra[j] != palavra[i])
+            //    j--;
+        }
+        if(palavra[j] == palavra[i]){
+            //printf("\nentrou no j %d %d\n", i, j+1);
+            prefix[i] = j+1;
+            j++;
+        }
+    }
+
+    int x;
+    return prefix;
+
+}
+
+
+
+
+
+void KMP(char texto[], char palavra[]){
+    int *F = prefixos(palavra);
+    int i = 0;
+    int j = 0;
+    int n = strlen(texto);
+    int m = strlen(palavra);
+    int find = 0;
+
+
+    while(i < n){
+        if(texto[i] == palavra[j]){
+            if(j == m-1 && (i - j == 0 || texto[i-j-1] == ' ') && (i == n-1 || texto[i+1] == ' ') ){
+                printf("%d ", i-j);
+                find++;
+                i++;
+                j = 0;
+            }else{
+                i++;
+                j++;
+             }
+        }else{
+            if(j>0)
+                j = F[j-1];
+            else
+                i++;
+        }
+    }
+    if(find != 0)printf("\n");
+    if(find == 0) printf("-1\n");
+}
+
+
+
+
+void main(){
+    clock_t start, end, total;
+    start = clock();
+
+    int num;
+    scanf("%d", &num);
+
+    char d[50];
+
+    char txt[10000];
+    char word[50];
+    while(num > 0){
+        txt[0] = '\0';
+        word[0] = '\0';
+        scanf("%c", d);
+        scanf("%[^\n]%*c", txt);
+        scanf("%s", word);
+        KMP(txt, word);
+        num--;
+    }
+
+    end = clock();
+
+
+
+
+}
